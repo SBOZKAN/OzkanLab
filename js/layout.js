91454696279639f09d2fe876f22814842e54d8b3
Sam Kazan <kzncem@gmail.com>
2024-05-09 11:33:08 -0700

    restructure

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   if (document.location.href.indexOf("index") < 0 ) {
-    $( "#loaded-layout" ).load( "../index.html #layout");
+    $( "#loaded-layout" ).load( "../boiler.html #layout");
   }
 });
